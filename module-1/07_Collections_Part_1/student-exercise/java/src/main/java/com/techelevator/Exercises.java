da0581e6555c2d28593f7c5393eccb4cab3f8fb9
william <william.hardy33@yahoo.com>
2020-12-09 13:05:17 -0500

    Submitting Week 2 day 2 excercise

--- module-1/07_Collections_Part_1/student-exercise/java/src/main/java/com/techelevator/Exercises.java
+++ module-1/07_Collections_Part_1/student-exercise/java/src/main/java/com/techelevator/Exercises.java
@@ -1,112 +1,205 @@
 package com.techelevator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class Exercises {
+public class Exercises 
+{
 
 	/*
-	 Note, for-each is preferred, and should be used when possible.
+	 * Note, for-each is preferred, and should be used when possible.
 	 */
 
 	/*
-	 Given an array of Strings, return an ArrayList containing the same Strings in the same order
-	 array2List( {"Apple", "Orange", "Banana"} )  ->  ["Apple", "Orange", "Banana"]
-	 array2List( {"Red", "Orange", "Yellow"} )  ->  ["Red", "Orange", "Yellow"]
-	 array2List( {"Left", "Right", "Forward", "Back"} )  ->  ["Left", "Right", "Forward", "Back"]
+	 * Given an array of Strings, return an ArrayList containing the same Strings in
+	 * the same order array2List( {"Apple", "Orange", "Banana"} ) -> ["Apple",
+	 * "Orange", "Banana"] array2List( {"Red", "Orange", "Yellow"} ) -> ["Red",
+	 * "Orange", "Yellow"] array2List( {"Left", "Right", "Forward", "Back"} ) ->
+	 * ["Left", "Right", "Forward", "Back"]
 	 */
-	public List<String> array2List(String[] stringArray) {
-		return null;
+	public List<String> array2List(String[] stringArray) 
+	{
+		List<String> strList = Arrays.asList(stringArray);
+		return strList;
 	}
 
 	/*
-	 Given a list of Strings, return an array containing the same Strings in the same order
-	 list2Array( ["Apple", "Orange", "Banana"] )  ->  {"Apple", "Orange", "Banana"}
-	 list2Array( ["Red", "Orange", "Yellow"] )  ->  {"Red", "Orange", "Yellow"}
-	 list2Array( ["Left", "Right", "Forward", "Back"] )  ->  {"Left", "Right", "Forward", "Back"}
+	 * Given a list of Strings, return an array containing the same Strings in the
+	 * same order list2Array( ["Apple", "Orange", "Banana"] ) -> {"Apple", "Orange",
+	 * "Banana"} list2Array( ["Red", "Orange", "Yellow"] ) -> {"Red", "Orange",
+	 * "Yellow"} list2Array( ["Left", "Right", "Forward", "Back"] ) -> {"Left",
+	 * "Right", "Forward", "Back"}
 	 */
-	public String[] list2Array(List<String> stringList) {
-		return null;
+	public String[] list2Array(List<String> stringList) 
+	{
+		String[] stringArr = stringList.toArray(new String[stringList.size()]);
+		return stringArr;
 	}
 
 	/*
-	 Given an array of Strings, return an ArrayList containing the same Strings in the same order
-	 except for any words that contain exactly 4 characters.
-	 no4LetterWords( {"Train", "Boat", "Car"} )  ->  ["Train", "Car"]
-	 no4LetterWords( {"Red", "White", "Blue"} )  ->  ["Red", "White"]
-	 no4LetterWords( {"Jack", "Jill", "Jane", "John", "Jim"} )  ->  ["Jim"]
+	 * Given an array of Strings, return an ArrayList containing the same Strings in
+	 * the same order except for any words that contain exactly 4 characters.
+	 * no4LetterWords( {"Train", "Boat", "Car"} ) -> ["Train", "Car"]
+	 * no4LetterWords( {"Red", "White", "Blue"} ) -> ["Red", "White"]
+	 * no4LetterWords( {"Jack", "Jill", "Jane", "John", "Jim"} ) -> ["Jim"]
 	 */
-	public List<String> no4LetterWords(String[] stringArray) {
-		return null;
+	public List<String> no4LetterWords(String[] stringArray) 
+	{
+		List<String> strList = new ArrayList<String>();
+		for (int i = 0; i < stringArray.length; i++) 
+		{
+			if (stringArray[i].length() != 4) 
+			{
+				strList.add(stringArray[i]);
+			}
+		}
+		return strList;
 	}
 
 	/*
-	 Given an array of ints, divide each int by 2, and return an ArrayList of Doubles.
-	 arrayInt2ListDouble( {5, 8, 11, 200, 97} ) -> [2.5, 4.0, 5.5, 100, 48.5]
-	 arrayInt2ListDouble( {745, 23, 44, 9017, 6} ) -> [372.5, 11.5, 22, 4508.5, 3]
-	 arrayInt2ListDouble( {84, 99, 3285, 13, 877} ) -> [42, 49.5, 1642.5, 6.5, 438.5]
+	 * Given an array of ints, divide each int by 2, and return an ArrayList of
+	 * Doubles. arrayInt2ListDouble( {5, 8, 11, 200, 97} ) -> [2.5, 4.0, 5.5, 100,
+	 * 48.5] arrayInt2ListDouble( {745, 23, 44, 9017, 6} ) -> [372.5, 11.5, 22,
+	 * 4508.5, 3] arrayInt2ListDouble( {84, 99, 3285, 13, 877} ) -> [42, 49.5,
+	 * 1642.5, 6.5, 438.5]
 	 */
-	public List<Double> arrayInt2ListDouble(int[] intArray) {
-		return null;
+	public List<Double> arrayInt2ListDouble(int[] intArray) 
+	{
+		List<Double> intList = new ArrayList<Double>();
+		for (int i = 0; i < intArray.length; i++) 
+		{
+			intList.add((double) intArray[i] / 2);
+		}
+		return intList;
 	}
 
 	/*
-	 Given a List of Integers, return the largest value.
-	 findLargest( [11, 200, 43, 84, 9917, 4321, 1, 33333, 8997] ) -> 33333
-	 findLargest( [987, 1234, 9381, 731, 43718, 8932] ) -> 43718
-	 findLargest( [34070, 1380, 81238, 7782, 234, 64362, 627] ) -> 64362
+	 * Given a List of Integers, return the largest value. findLargest( [11, 200,
+	 * 43, 84, 9917, 4321, 1, 33333, 8997] ) -> 33333 findLargest( [987, 1234, 9381,
+	 * 731, 43718, 8932] ) -> 43718 findLargest( [34070, 1380, 81238, 7782, 234,
+	 * 64362, 627] ) -> 64362
 	 */
-	public Integer findLargest(List<Integer> integerList) {
-		return null;
+	public Integer findLargest(List<Integer> integerList) 
+	{
+		List<Integer> sortedList = integerList;
+		sortedList.sort(null);
+		return sortedList.get(sortedList.size() - 1);
 	}
 
 	/*
-	 Given an array of Integers, return a List of Integers containing just the odd values.
-	 oddOnly( {112, 201, 774, 92, 9, 83, 41872} ) -> [201, 9, 83]
-	 oddOnly( {1143, 555, 7, 1772, 9953, 643} ) -> [1143, 555, 7, 9953, 643]
-	 oddOnly( {734, 233, 782, 811, 3, 9999} ) -> [233, 811, 3, 9999]
+	 * Given an array of Integers, return a List of Integers containing just the odd
+	 * values. oddOnly( {112, 201, 774, 92, 9, 83, 41872} ) -> [201, 9, 83] oddOnly(
+	 * {1143, 555, 7, 1772, 9953, 643} ) -> [1143, 555, 7, 9953, 643] oddOnly( {734,
+	 * 233, 782, 811, 3, 9999} ) -> [233, 811, 3, 9999]
 	 */
-	public List<Integer> oddOnly(Integer[] integerArray) {
-		return null;
+	public List<Integer> oddOnly(Integer[] integerArray) 
+	{
+		List<Integer> oddList = new ArrayList<Integer>();
+		for (Integer elmt : integerArray) 
+		{
+			if (elmt % 2 != 0) 
+			{
+				oddList.add(elmt);
+			}
+		}
+		return oddList;
 	}
 
 	/*
-	 Given a List of Integers, and an int value, return true if the int value appears two or more times in
-	 the list.
-	 foundIntTwice( [5, 7, 9, 5, 11], 5 ) -> true
-	 foundIntTwice( [6, 8, 10, 11, 13], 8 -> false
-	 foundIntTwice( [9, 23, 44, 2, 88, 44], 44) -> true
+	 * Given a List of Integers, and an int value, return true if the int value
+	 * appears two or more times in the list. foundIntTwice( [5, 7, 9, 5, 11], 5 )
+	 * -> true foundIntTwice( [6, 8, 10, 11, 13], 8 -> false foundIntTwice( [9, 23,
+	 * 44, 2, 88, 44], 44) -> true
 	 */
-	public boolean foundIntTwice(List<Integer> integerList, int intToFind) {
-		return false;
+	public boolean foundIntTwice(List<Integer> integerList, int intToFind) 
+	{
+		int count = 0;
+		for (int intItem : integerList) 
+		{
+			if (intItem == intToFind) 
+			{
+				count++;
+			}
+		}
+		return count >= 2;
 	}
 
 	/*
-	 Given an array of Integers, return a List that contains the same Integers (as Strings). Except any multiple of 3
-	should be replaced by the String "Fizz", any multiple of 5 should be replaced by the String "Buzz",
-	and any multiple of both 3 and 5 should be replaced by the String "FizzBuzz"
-	** INTERVIEW QUESTION **
-	
-	fizzBuzzList( {1, 2, 3} )  ->  [1, 2, "Fizz"]
-	 fizzBuzzList( {4, 5, 6} )  ->  [4, "Buzz", 6]
-	 fizzBuzzList( {7, 8, 9, 10, 11, 12, 13, 14, 15} )  ->  [7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
-	
-	 HINT: To convert an integer x to a string you can call x.toString() in your code (e.g. if x = 1 then x.ToString()
-	 equals "1")
+	 * Given an array of Integers, return a List that contains the same Integers (as
+	 * Strings). Except any multiple of 3 should be replaced by the String "Fizz",
+	 * any multiple of 5 should be replaced by the String "Buzz", and any multiple
+	 * of both 3 and 5 should be replaced by the String "FizzBuzz" INTERVIEW
+	 * QUESTION **
+	 * 
+	 * fizzBuzzList( {1, 2, 3} ) -> [1, 2, "Fizz"] fizzBuzzList( {4, 5, 6} ) -> [4,
+	 * "Buzz", 6] fizzBuzzList( {7, 8, 9, 10, 11, 12, 13, 14, 15} ) -> [7, 8,
+	 * "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+	 * 
+	 * HINT: To convert an integer x to a string you can call x.toString() in your
+	 * code (e.g. if x = 1 then x.ToString() equals "1")
 	 */
-	public List<String> fizzBuzzList(Integer[] integerArray) {
-		return null;
+	public List<String> fizzBuzzList(Integer[] integerArray) 
+	{
+		List<String> strList = new ArrayList<String>();
+		for (int intItem : integerArray) 
+		{
+			String fizzBuzz = "";
+			if (intItem % 3 == 0) 
+			{
+				fizzBuzz += "Fizz";
+			}
+			if (intItem % 5 == 0) 
+			{
+				fizzBuzz += "Buzz";
+			}
+			if (fizzBuzz.equals("")) 
+			{
+				strList.add(Integer.toString(intItem));
+			} 
+			else 
+			{
+				strList.add(fizzBuzz);
+			}
+		}
+		return strList;
 	}
 
 	/*
-	 Given two lists of Integers, interleave them beginning with the first element in the first list followed
-	 by the first element of the second. Continue interleaving the elements until all elements have been interwoven.
-	 Return the new list. If the lists are of unequal lengths, simply attach the remaining elements of the longer
-	 list to the new list before returning it.
-	 interleaveLists( [1, 2, 3], [4, 5, 6] )  ->  [1, 4, 2, 5, 3, 6]
+	 * Given two lists of Integers, interleave them beginning with the first element
+	 * in the first list followed by the first element of the second. Continue
+	 * interleaving the elements until all elements have been interwoven. Return the
+	 * new list. If the lists are of unequal lengths, simply attach the remaining
+	 * elements of the longer list to the new list before returning it.
+	 * interleaveLists( [1, 2, 3], [4, 5, 6] ) -> [1, 4, 2, 5, 3, 6]
 	 */
-	public List<Integer> interleaveLists(List<Integer> listOne, List<Integer> listTwo) {
-		return null;
+	public List<Integer> interleaveLists(List<Integer> listOne, List<Integer> listTwo) 
+	{
+		int maxLength;
+		List<Integer> interList = new ArrayList<Integer>();
+		
+		if (listOne.size() >= listTwo.size()) 
+		{
+			maxLength = listOne.size();
+		} 
+		else 
+		{
+			maxLength = listTwo.size();
+		}
+		
+		for (int i = 0; i < maxLength; i++) 
+		{
+			if (i < listOne.size()) 
+			{
+				interList.add(listOne.get(i));
+			}
+
+			if (i < listTwo.size()) 
+			{
+				interList.add(listTwo.get(i));
+			}
+		}
+		return interList;
 	}
 
 }
