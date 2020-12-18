059131e4ea7daf42a636feaddf7be663c53e7664
Gregor Dzierzon <gdzierzon@gmail.com>
2020-12-18 03:29:11 -0700

    added application and ui packages to card game

---
+++ module-1/15_Review/card-games-inheritance/java/src/main/java/com/techelevator/application/Games.java
@@ -0,0 +1,14 @@
+package com.techelevator.application;
+
+import com.techelevator.ui.UserOutput;
+
+public class Games 
+{
+    UserOutput output = new UserOutput();
+
+    public void run()
+    {
+        output.displaySplashScreen();
+    }
+    
+}
