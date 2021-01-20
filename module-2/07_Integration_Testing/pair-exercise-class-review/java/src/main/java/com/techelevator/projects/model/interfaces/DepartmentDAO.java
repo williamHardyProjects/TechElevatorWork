0706763dc351ef78ae66389f1f6280c7fd401d65
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2021-01-20 13:33:07 -0700

    added pair exercise review code

---
+++ module-2/07_Integration_Testing/pair-exercise-class-review/java/src/main/java/com/techelevator/projects/model/interfaces/DepartmentDAO.java
@@ -0,0 +1,48 @@
+package com.techelevator.projects.model.interfaces;
+
+import java.util.List;
+
+import com.techelevator.projects.model.Department;
+
+public interface DepartmentDAO {
+
+	/**
+	 * Get all departments from the datastore.
+	 * 
+	 * @return all departments as Department objects in a List
+	 */
+	public List<Department> getAllDepartments();
+
+	/**
+	 * Get all the departments whose name contains the search string. Remember
+	 * to use LIKE or ILIKE to do a fuzzy match on the search.
+	 * 
+	 * @param nameSearch the search string to look for in the department name
+	 * @return all matching departments as Department objects in a List
+	 */
+	public List<Department> searchDepartmentsByName(String nameSearch);
+
+	/**
+	 * Update a department to the datastore. Only called on departments that
+	 * are already in the datastore.
+	 * 
+	 * @param updatedDepartment the department object to update
+	 */
+	public void saveDepartment(Department updatedDepartment);
+
+	/**
+	 * Inserts a new department into the datastore.
+	 * 
+	 * @param newDepartment the department object to insert
+	 * @return the department object with its new id filled in
+	 */
+	public Department createDepartment(Department newDepartment);
+
+	/**
+	 * Get a department from the datastore that belongs to the given id.
+	 * 
+	 * @param id the department id to get from the datastore
+	 * @return a filled out department object
+	 */
+	public Department getDepartmentById(Long id);
+}
