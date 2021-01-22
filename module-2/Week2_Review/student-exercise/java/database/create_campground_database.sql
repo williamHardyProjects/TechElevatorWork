f0e2162729b24718052ed51646233453258f2c66
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2021-01-22 00:59:35 -0700

    modified campground database scripts

--- module-2/Week2_Review/student-exercise/java/database/create_campground_database.sql
+++
@@ -1,7 +0,0 @@
--- connected to the POSTGRES database when you are creating another db
--- kill and db locks
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'campground';
-
-DROP DATABASE campground;
-
-CREATE DATABASE campground;
