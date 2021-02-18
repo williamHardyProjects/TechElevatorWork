8de8bf3c527c6b02d878ff269eb1f283dc4ca7a8
william <william.hardy33@yahoo.com>
2021-02-18 08:27:35 -0500

    completed module 3 exercise 22

--- module-3/22_DOM/student-exercise/shopping-list/js/shopping-list.js
+++ module-3/22_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -1,23 +1,40 @@
 // add pageTitle
-
+const pageTitle = 'My Shopping List';
+const groceries = ['rice', 'salmon', 'beans', 'chicken', 'adobo seasoning', 'flank steak', 'broccoli', 'red bell peppers', 'black pepper', 'yams'];
 // add groceries
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+  document.querySelector('#title').innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+  let groceryList = document.querySelector('#groceries');
+
+  let domFragment = groceries.reduce((fragment, item) => {
+    let li = document.createElement('li')
+    li.appendChild(document.createTextNode(item))
+    fragment.appendChild(li);
+    return fragment;
+  }, new DocumentFragment())
+
+  groceryList.appendChild(domFragment);
+}
 
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  let groceryList = [...document.querySelectorAll('#groceries li')];
+  groceryList.map(item => item.classList.add('completed'));
+}
 
 setPageTitle();
 
